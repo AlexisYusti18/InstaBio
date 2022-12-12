74309191933df6f42ee12bdd0e5d707354603e18
Alexis Yusti <alexisyusti18@gmail.com>
2022-12-12 16:10:00 -0300

    INSTABIO

---
+++ src/components/Hero.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from '../assets/logo.jpg'
+import catalogo from '../assets/catalogo.pdf'
+
+export default function Hero() {
+  return (
+    <div className='ctn-hero'>
+        <div className='ctn-title'>
+            <img src={logo} alt='logo'/>
+            <h1>LA PREFERIDA</h1>
+            <p>Navarro, Bs As</p>
+            <button className='btn'>
+                <a href={catalogo} Catalogo={' '}>Descargar Catalogo</a>
+            </button>
+            <div className='ctn-social'>
+                <button className='btn'>
+                    <a href='https://www.instagram.com/preferida.la/'>Instagram</a>
+                </button>
+                <button className='btn'>
+                    <a href='https://www.instagram.com/preferida.la/'>Facebook</a>
+                </button>
+                <button className='btn'>
+                    <a href='https://www.instagram.com/preferida.la/'>Whatsapp</a>
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
