daec330479d1d766c93be07717e1c42cbab75869
Alexis Yusti <alexisyusti18@gmail.com>
2023-01-24 02:25:41 -0300

    agregue icons

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import logo from '../assets/logo.jpg'
 import menu from '../assets/menu.pdf'
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export default function Hero() {
   return (
@@ -8,19 +13,23 @@ export default function Hero() {
         <div className='ctn-title'>
             <img src={logo} alt='logo'/>
             <h3>LA PREFERIDA</h3>
-            <p>📍Ubicados en Navarro</p>
+            <p><FmdGoodIcon sx={{color:'red'}}/>Navarro</p>
             <div className='ctn-links'>
                 <button className='btn'>
+                    <RestaurantIcon/>
                     <a href={menu} catalogo={' '}>Menu</a>
                 </button>
                 <button className='btn'>
+                    <InstagramIcon/>    
                     <a href='https://www.instagram.com/preferida.la/'>Instagram</a>
                 </button>
                 <button className='btn'>
-                    <a href='https://www.instagram.com/preferida.la/'>Facebook</a>
+                    <FacebookIcon/> 
+                    <a href='https://www.facebook.com/profile.php?id=100063723622977'>Facebook</a>
                 </button>
                 <button className='btn'>
-                    <a href='https://www.instagram.com/preferida.la/'>Whatsapp</a>
+                    <WhatsAppIcon/>
+                    <a href='https://wa.me/c/5492227511940'>Whatsapp</a>
                 </button>
             </div>
         </div>
